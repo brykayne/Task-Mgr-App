6b650a2876fd7c628b9ea7411dcf4ec96d91a29f
Bryan Kayne <brykayne@gmail.com>
2017-07-25 10:20:02 -0400

    Added select Board functionality and refactored code to be more reliant on appData object. Added styling to added boards/cards.

--- dist/bundle.js
+++ dist/bundle.js
@@ -57,32 +57,44 @@
 	    var appData = {
 	        boards: boards,
 	        selectedBoard: boards[Object.keys(boards)[0]],
-	        selectedBoardEl: null
+	        selectedBoardEl: null,
+	        selectedCard: null,
+	        selectedCardEl: null
 	    };
 	    setListeners(appData);
-	    boardsToView(appData.boards);
-	    selectedBoardToView(appData.selectedBoard);
-	    selectBoard(document.getElementById('myBoards').firstElementChild, appData);
+	    boardsToView(appData);
+	    selectedBoardToView(appData);
 	}
 
 	function setListeners(appData) {
 	    document.getElementById('myBoardsForm').addEventListener('submit', function (event) {
 	        event.preventDefault();
-	        console.log(event);
 	        addBoard(createBoard(event.target.myboardsInput.value), appData);
+	        event.target.reset();
 	    });
 
 	    document.getElementById('cardForm').addEventListener('submit', function (event) {
 	        event.preventDefault();
 	        addCard(createCard(event.target.cardInput.value), appData);
+	        event.target.reset();
 	    });
 
 	    document.getElementById('myBoards').addEventListener('click', function (event) {
 	        event.preventDefault();
 	        if (event.target.tagName === 'LI') {
-	            console.log(event.target.getAttribute('data-board'));
 	            selectBoard(event.target, appData);
 	        }
+	    });
+
+	    document.getElementById('selectedBoard').addEventListener('click', function (event) {
+	        //41
+
+	        //Add unique id to each card: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+	        // if(event.target.tagName === 'LI') {
+	        //     // selectCard(event.target,)
+	        //     console.log('woah! Board LI hit.')
+	        //
+	        // }
 	    });
 
 	    document.getElementById('forwardBtn').addEventListener('click', function (event) {
@@ -112,17 +124,21 @@
 	    return boards;
 	}
 
-	function boardsToView(boards) {
+	function boardsToView(appData) {
 	    var myBoardsEl = document.getElementById('myBoards');
-	    for (var board in boards) {
-	        myBoardsEl.appendChild(boardtoBoardEl(boards[board]));
-	    }
+	    for (var board in appData.boards) {
+	        myBoardsEl.appendChild(boardtoBoardEl(appData.boards[board]));
+	    };
+
+	    appData.selectedBoardEl = myBoardsEl.firstElementChild;
+	    addClass(appData.selectedBoardEl, 'active-board-item-selected');
+	    console.log(appData.selectedBoardEl);
 	}
 
 	function boardtoBoardEl(board) {
 	    var boardEl = document.createElement('li');
 	    boardEl.innerHTML = board.name;
-	    boardEl.setAttribute('data-board', JSON.stringify(board));
+	    boardEl.setAttribute('data-board-name', board.name);
 	    addClass(boardEl, "myboards-el");
 	    return boardEl;
 	}
@@ -136,16 +152,14 @@
 
 	function addBoard(board, appData) {
 	    appData.boards[board.name] = { name: board.name, cards: [] };
-	    console.log(appData);
 	    var myBoardsEl = document.getElementById('myBoards');
 	    var boardEl = boardtoBoardEl(board);
-	    //Add fade-in class here...
 	    addClass(boardEl, 'fade-in');
 	    myBoardsEl.appendChild(boardEl);
 	    selectBoard(boardEl, appData);
 	}
 
-	function selectedBoardToView(selectedBoard) {
+	function selectedBoardToView(appData) {
 	    var todoCardsEl = document.getElementById('toDoCards');
 	    var inProgressCardsEl = document.getElementById('inProgressCards');
 	    var doneCardsEl = document.getElementById('doneCards');
@@ -153,23 +167,31 @@
 	    inProgressCardsEl.innerHTML = '';
 	    doneCardsEl.innerHTML = '';
 
-	    for (var card in selectedBoard.cards) {
-	        console.log(selectedBoard.cards[card]);
-	        if (selectedBoard.cards[card].position == 0) {
-	            todoCardsEl.appendChild(cardToCardEl(selectedBoard.cards[card]));
-	        } else if (selectedBoard.cards[card].position == 1) {
-	            inProgressCardsEl.appendChild(cardToCardEl(selectedBoard.cards[card]));
-	        } else if (selectedBoard.cards[card].position == 2) {
-	            doneCardsEl.appendChild(cardToCardEl(selectedBoard.cards[card]));
-	        } else {
-	            console.log('Position could not be found');
+	    //Might need if(!appData.selectedList.items[item].isDeleted)
+
+
+	    for (var _card in appData.selectedBoard.cards) {
+	        console.log(appData.selectedBoard.cards[_card]);
+	        if (!appData.selectedBoard.cards[_card].isDeleted) {
+	            if (appData.selectedBoard.cards[_card].position == 0) {
+	                todoCardsEl.appendChild(cardToCardEl(appData.selectedBoard.cards[_card]));
+	            } else if (appData.selectedBoard.cards[_card].position == 1) {
+	                inProgressCardsEl.appendChild(cardToCardEl(appData.selectedBoard.cards[_card]));
+	            } else if (appData.selectedBoard.cards[_card].position == 2) {
+	                doneCardsEl.appendChild(cardToCardEl(appData.selectedBoard.cards[_card]));
+	            } else {
+	                console.log('Position could not be found');
+	            };
 	        };
 	    };
 	}
 
-	function cardToCardEl(card) {
+	function cardToCardEl(card, arPosition) {
 	    var cardEl = document.createElement('li');
 	    cardEl.innerHTML = card.name;
+	    cardEl.setAttribute('data-card', JSON.stringify(card));
+	    cardEl.setAttribute('data-ar-pos', arPosition);
+
 	    addClass(cardEl, 'card-el');
 	    return cardEl;
 	}
@@ -186,32 +208,50 @@
 
 	function addCard(card, appData) {
 	    appData.selectedBoard.cards.push(card);
-	    appData.selectedBoardEl.setAttribute('data-board', JSON.stringify(appData.selectedBoard));
-	    console.log(JSON.stringify(appData.selectedBoard));
-	    console.log(appData);
+	    // appData.selectedBoardEl.setAttribute('data-board',JSON.stringify(appData.selectedBoard));
+	    // console.log(appData);
 	    var todoCardsEl = document.getElementById('toDoCards');
-	    var cardEl = cardToCardEl(card);
+	    var cardEl = cardToCardEl(card, appData.selectedBoard.cards.length - 1);
 	    addClass(cardEl, 'fade-in');
 	    todoCardsEl.appendChild(cardEl);
+	    // selectCard(cardEl, appData);
+	    window.setTimeout(function () {
+	        removeClass(cardEl, 'fade-in');
+	    }, 200);
 	}
 
 	function selectBoard(boardEl, appData) {
+	    debugger;
+	    appData.selectedCardEl = null; //162
+	    appData.selectedCard = null;
+	    appData.selectedBoard = appData.boards[boardEl.getAttribute('data-board-name')];
 	    if (appData.selectedBoardEl != null) {
 	        removeClass(appData.selectedBoardEl, 'active-board-item-selected');
 	    }
 	    appData.selectedBoardEl = boardEl;
-	    addClass(boardEl, 'active-board-item-selected');
-	    appData.selectedBoard = JSON.parse(boardEl.getAttribute('data-board'));
-	    selectedBoardToView(appData.selectedBoard);
+	    addClass(appData.selectedBoardEl, 'active-board-item-selected');
+	    // appData.selectedBoard = JSON.parse(boardEl.getAttribute('data-board-name'));
+	    selectedBoardToView(appData);
 	}
 
-	// function cardForward(cardEl, appData) {
-	//
-	// }
-	//
-	// function cardBackward(cardEl, appData) {
-	//
-	// }
+	function selectCard(cardEl, appData) {
+	    console.log('Card selected');
+	    if (appData.selectedCardEl != null) {
+	        removeClass(appData.selectedCardEl, 'active-board-item-selected');
+	    };
+	    appData.selectedCardEl = cardEl;
+	    addClass(cardEl, 'active-board-item-selected');
+	    appData.selectedCard = JSON.parse(card.getAttribute('data-card'));
+	    //card details view
+	}
+
+	function deleteCard(cardEl, appData) {
+	    appData.selectedBoard.cards[cardEl.getAttribute('data-ar-pos')].isDeleted = true;
+	    addClass(cardEl, 'fadeout-el');
+	    window.setTimeout(function () {
+	        cardEl.remove();
+	    }, 500);
+	}
 
 /***/ })
 /******/ ]);
--- scripts/app.js
+++ scripts/app.js
@@ -9,23 +9,26 @@ function main() {
     let appData = {
         boards: boards,
         selectedBoard: boards[Object.keys(boards)[0]],
-        selectedBoardEl: null
+        selectedBoardEl: null,
+        selectedCard: null,
+        selectedCardEl: null
     };
     setListeners(appData);
-    boardsToView(appData.boards);
-    selectedBoardToView(appData.selectedBoard);
-    selectBoard(document.getElementById('myBoards').firstElementChild, appData);
+    boardsToView(appData);
+    selectedBoardToView(appData);
 }
 
 function setListeners(appData) {
     document.getElementById('myBoardsForm').addEventListener('submit', function(event) {
         event.preventDefault();
         addBoard(createBoard(event.target.myboardsInput.value), appData);
+        event.target.reset();
     });
 
     document.getElementById('cardForm').addEventListener('submit', function(event) {
         event.preventDefault();
         addCard(createCard(event.target.cardInput.value), appData);
+        event.target.reset();
     });
 
     document.getElementById('myBoards').addEventListener('click', function(event) {
@@ -34,6 +37,17 @@ function setListeners(appData) {
             selectBoard(event.target, appData);
         }
     });
+
+    document.getElementById('selectedBoard').addEventListener('click', function(event) {
+        //41
+
+        //Add unique id to each card: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+        // if(event.target.tagName === 'LI') {
+        //     // selectCard(event.target,)
+        //     console.log('woah! Board LI hit.')
+        //
+        // }
+    })
 
     document.getElementById('forwardBtn').addEventListener('click', function(event) {
         event.preventDefault();
@@ -71,17 +85,21 @@ function getBoards() {
     return boards;
 }
 
-function boardsToView(boards) {
+function boardsToView(appData) {
     let myBoardsEl = document.getElementById('myBoards');
-    for(let board in boards) {
-        myBoardsEl.appendChild(boardtoBoardEl(boards[board]));
-    }
+    for(let board in appData.boards) {
+        myBoardsEl.appendChild(boardtoBoardEl(appData.boards[board]));
+    };
+
+    appData.selectedBoardEl = myBoardsEl.firstElementChild;
+    addClass(appData.selectedBoardEl, 'active-board-item-selected');
+    console.log(appData.selectedBoardEl);
 }
 
 function boardtoBoardEl(board) {
     let boardEl = document.createElement('li');
     boardEl.innerHTML = board.name;
-    boardEl.setAttribute('data-board', JSON.stringify(board));
+    boardEl.setAttribute('data-board-name', board.name);
     addClass(boardEl, "myboards-el");
     return boardEl;
 }
@@ -97,13 +115,12 @@ function addBoard(board, appData) {
     appData.boards[board.name] = {name:board.name, cards:[]};
     let myBoardsEl = document.getElementById('myBoards');
     let boardEl = boardtoBoardEl(board);
-    //Add fade-in class here...
     addClass(boardEl, 'fade-in');
     myBoardsEl.appendChild(boardEl);
     selectBoard(boardEl, appData);
 }
 
-function selectedBoardToView(selectedBoard) {
+function selectedBoardToView(appData) {
     let todoCardsEl = document.getElementById('toDoCards');
     let inProgressCardsEl = document.getElementById('inProgressCards');
     let doneCardsEl = document.getElementById('doneCards');
@@ -111,23 +128,32 @@ function selectedBoardToView(selectedBoard) {
     inProgressCardsEl.innerHTML = '';
     doneCardsEl.innerHTML = '';
 
-    for(let card in selectedBoard.cards) {
-        console.log(selectedBoard.cards[card]);
-        if (selectedBoard.cards[card].position == 0) {
-            todoCardsEl.appendChild(cardToCardEl(selectedBoard.cards[card]));
-        } else if (selectedBoard.cards[card].position == 1) {
-            inProgressCardsEl.appendChild(cardToCardEl(selectedBoard.cards[card]));
-        } else if (selectedBoard.cards[card].position == 2) {
-            doneCardsEl.appendChild(cardToCardEl(selectedBoard.cards[card]));
-        } else {
-            console.log('Position could not be found');
+    //Might need if(!appData.selectedList.items[item].isDeleted)
+
+
+
+    for(let card in appData.selectedBoard.cards) {
+        console.log(appData.selectedBoard.cards[card]);
+        if(!appData.selectedBoard.cards[card].isDeleted) {
+            if (appData.selectedBoard.cards[card].position == 0) {
+                todoCardsEl.appendChild(cardToCardEl(appData.selectedBoard.cards[card]));
+            } else if (appData.selectedBoard.cards[card].position == 1) {
+                inProgressCardsEl.appendChild(cardToCardEl(appData.selectedBoard.cards[card]));
+            } else if (appData.selectedBoard.cards[card].position == 2) {
+                doneCardsEl.appendChild(cardToCardEl(appData.selectedBoard.cards[card]));
+            } else {
+                console.log('Position could not be found');
+            };
         };
     };
 }
 
-function cardToCardEl(card) {
+function cardToCardEl(card, arPosition) {
     let cardEl = document.createElement('li');
     cardEl.innerHTML = card.name;
+    cardEl.setAttribute('data-card', JSON.stringify(card));
+    cardEl.setAttribute('data-ar-pos', arPosition);
+
     addClass(cardEl, 'card-el');
     return cardEl;
 }
@@ -144,30 +170,43 @@ function createCard(name) {
 
 function addCard(card, appData) {
     appData.selectedBoard.cards.push(card);
-    appData.selectedBoardEl.setAttribute('data-board',JSON.stringify(appData.selectedBoard));
-    console.log(appData);
+    // appData.selectedBoardEl.setAttribute('data-board',JSON.stringify(appData.selectedBoard));
+    // console.log(appData);
     let todoCardsEl = document.getElementById('toDoCards');
-    let cardEl = cardToCardEl(card);
+    let cardEl = cardToCardEl(card, appData.selectedBoard.cards.length-1);
     addClass(cardEl, 'fade-in');
     todoCardsEl.appendChild(cardEl);
+    // selectCard(cardEl, appData);
+    window.setTimeout(function(){removeClass(cardEl, 'fade-in')}, 200);
 }
 
 function selectBoard(boardEl, appData) {
+    debugger;
+    appData.selectedCardEl = null; //162
+    appData.selectedCard = null;
+    appData.selectedBoard = appData.boards[boardEl.getAttribute('data-board-name')];
     if(appData.selectedBoardEl != null) {
         removeClass(appData.selectedBoardEl, 'active-board-item-selected');
     }
     appData.selectedBoardEl = boardEl;
-    addClass(boardEl, 'active-board-item-selected');
-    appData.selectedBoard = JSON.parse(boardEl.getAttribute('data-board'));
-    selectedBoardToView(appData.selectedBoard);
+    addClass(appData.selectedBoardEl, 'active-board-item-selected');
+    // appData.selectedBoard = JSON.parse(boardEl.getAttribute('data-board-name'));
+    selectedBoardToView(appData);
 }
 
+function selectCard(cardEl, appData) {
+    console.log('Card selected');
+    if (appData.selectedCardEl != null) {
+        removeClass(appData.selectedCardEl, 'active-board-item-selected');
+    };
+    appData.selectedCardEl = cardEl;
+    addClass(cardEl, 'active-board-item-selected');
+    appData.selectedCard = JSON.parse(card.getAttribute('data-card'));
+    //card details view
+}
 
-
-// function cardForward(cardEl, appData) {
-//
-// }
-//
-// function cardBackward(cardEl, appData) {
-//
-// }
+function deleteCard(cardEl, appData) {
+    appData.selectedBoard.cards[cardEl.getAttribute('data-ar-pos')].isDeleted = true;
+    addClass(cardEl, 'fadeout-el');
+    window.setTimeout(function(){cardEl.remove();},500);
+}
